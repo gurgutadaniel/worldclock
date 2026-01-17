89eac284eda2abd3512a6301961b5fe9e8105944
Daniel Byte <gurgutadanielv@gmail.com>
2026-01-17 05:50:28 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const zones = {
+  bruxelles: "Europe/Brussels",
+  norfolk: "America/New_York",
+  kiev: "Europe/Kyiv",
+  utc: "UTC"
+};
+
+function formatTime(date, timeZone) {
+  return new Intl.DateTimeFormat("en-GB", {
+    timeZone: timeZone,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+  }).format(date);
+}
+
+function updateClocks() {
+  const now = new Date();
+
+  for (const [id, tz] of Object.entries(zones)) {
+    document.getElementById(id).textContent = formatTime(now, tz);
+  }
+}
+
+updateClocks();
+setInterval(updateClocks, 1000);
